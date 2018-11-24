fc4480d8bcac02aa9323a141f9592f06a23e33df
Thana Annis <thanaannis@gmail.com>
2018-11-24 13:02:42 -0500

    Added ability to search and retrieve their text

--- MediaTracker/Home/HomeViewModel.cs
+++ MediaTracker/Home/HomeViewModel.cs
@@ -1,10 +1,43 @@
 ﻿using MediaTracker.Common;
 using MediaTracker.Models;
+using System.Windows.Input;
 
 namespace MediaTracker.Home
 {
     public class HomeViewModel : ObservableObject, IPageViewModel
     {
+        private ICommand searchCommand;
+        private string searchText;
+
         public string Name => "Home";
+
+        public ICommand SearchClick
+        {
+            get
+            {
+                if (searchCommand == null)
+                    searchCommand = new RelayCommand(param => Search());
+
+                return searchCommand;
+            }
+        }
+
+        public string SearchText
+        {
+            get { return searchText; }
+            set
+            {
+                if (value != searchText)
+                {
+                    searchText = value;
+                    OnPropertyChanged("SearchText");
+                }
+            }
+        }
+
+        private void Search()
+        {
+            //search movies for their text
+        }
     }
 }
