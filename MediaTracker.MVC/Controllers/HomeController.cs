fda14f1e4b4dc4535672936012bd303cf89cafee
Thana Annis <thanaannis@gmail.com>
2018-12-08 16:51:33 -0500

    Moved movie logic to MovieController

--- MediaTracker.MVC/Controllers/HomeController.cs
+++ MediaTracker.MVC/Controllers/HomeController.cs
@@ -1,24 +1,12 @@
-﻿using Application.Movies.Queries.Search;
-using System.Linq;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MediaTracker.MVC.Controllers
 {
     public class HomeController : Controller
     {
-        private ISearchMovieQuery searchMovieQuery;
-
-        public HomeController(ISearchMovieQuery searchMovieQuery)
+        public ActionResult Index()
         {
-            this.searchMovieQuery = searchMovieQuery;
-        }
-
-        public ActionResult Index(string searchString)
-        {
-            if (searchString == null)
-                return View(Enumerable.Empty<SearchListItemModel>());
-
-            return View(searchMovieQuery.Execute(searchString));
+            return View();
         }
     }
 }
---
+++ MediaTracker.MVC/Controllers/MovieController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Movies.Queries.Search;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace MediaTracker.MVC.Controllers
+{
+    public class MovieController : Controller
+    {
+        private ISearchMovieQuery searchMovieQuery;
+
+        public MovieController(ISearchMovieQuery searchMovieQuery)
+        {
+            this.searchMovieQuery = searchMovieQuery;
+        }
+
+        public ActionResult Index(string searchString)
+        {
+            if (searchString == null)
+                return View(Enumerable.Empty<SearchListItemModel>());
+
+            return View(searchMovieQuery.Execute(searchString));
+        }
+    }
+}
