36ffb1a03edb5176993efa5a8673bd169bb91dc4
Thana Annis <thanaannis@gmail.com>
2018-12-14 21:40:41 -0500

    Added font awesome icons to project

--- MediaTracker.MVC/App_Start/BundleConfig.cs
+++ MediaTracker.MVC/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace MediaTracker.MVC
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/all.min.css"));
         }
     }
 }
